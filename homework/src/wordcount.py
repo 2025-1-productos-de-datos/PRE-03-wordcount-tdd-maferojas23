7abcfafa5e2667de45c4d947009fb259c1a2060b
maferojas23 <mafrojasva@unal.edu.co>
2025-05-10 09:41:03 -0500

    refactor rad_lines

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,34 @@
+#Ejemplo del caso de uso
+#python3 -m homework data/input data/output   
+## wordcount.py
 
+import argparse
+import sys
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Count words in files.")
+
+    parser.add_argument(
+        "input",
+        type=str,
+        help="Path to the input folder containing files to process",
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Path to the output folder for results",
+    )
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args.input, parsed_args.output
+
+   
+def main():
+    input_folder, output_folder = parse_args()
+
+    print("input foder:", input_folder)
+    print("output folder:", output_folder)
+        
