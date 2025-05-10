6f37f3a81e86126d04eb15b63f209d395377a81d
maferojas23 <mafrojasva@unal.edu.co>
2025-05-10 10:25:44 -0500

    primer intento taller 2

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,34 +1,19 @@
-#Ejemplo del caso de uso
-#python3 -m homework data/input data/output   
-## wordcount.py
-
-import argparse
-import sys
+# Ejemplo del caso de uso:
+# python3 -m homework --input data/input --output data/output
 
 
-def parse_args():
+from homework.src._internals.count_words import count_words
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
-    parser = argparse.ArgumentParser(description="Count words in files.")
 
-    parser.add_argument(
-        "input",
-        type=str,
-        help="Path to the input folder containing files to process",
-    )
-    parser.add_argument(
-        "output",
-        type=str,
-        help="Path to the output folder for results",
-    )
-
-    parsed_args = parser.parse_args()
-
-    return parsed_args.input, parsed_args.output
-
-   
 def main():
     input_folder, output_folder = parse_args()
-
-    print("input foder:", input_folder)
-    print("output folder:", output_folder)
-        
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
